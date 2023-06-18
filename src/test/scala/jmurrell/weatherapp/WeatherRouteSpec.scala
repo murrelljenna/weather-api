f9dfd200a60292f2b4e52e3c3fcb6437715b1aeb
Jenna Murrell <murrelljenna@gmail.com>
2023-06-18 18:50:58 -0400

    Add first WeatherRoute test

--- src/test/scala/jmurrell/weatherapp/WeatherRouteSpec.scala
+++ src/test/scala/jmurrell/weatherapp/WeatherRouteSpec.scala
@@ -1,23 +1,44 @@
 package jmurrell.weatherapp
 
 import cats.effect.IO
+import cats.implicits.toShow
+import jmurrell.weatherapp.OpenWeatherClient.{Temperature, WeatherCondition, WeatherResponse}
+import jmurrell.weatherapp.WeatherappRoutes.{Latitude, Longitude}
 import org.http4s._
 import org.http4s.implicits._
 import munit.CatsEffectSuite
 
 class WeatherRouteSpec extends CatsEffectSuite {
-
-  /*test("HelloWorld returns status code 200") {
-    assertIO(retHelloWorld.map(_.status) ,Status.Ok)
+  private[this] def mockWeatherClient(res: WeatherResponse) = new OpenWeatherClient {
+    override def get(lat: Latitude, lon: Longitude): IO[WeatherResponse] = IO.pure(
+      res.copy(lat = lat, lon = lon)
+    )
   }
 
-  test("HelloWorld returns hello world message") {
-    assertIO(retHelloWorld.flatMap(_.as[String]), "{\"message\":\"Hello, world\"}")
+  private[this] def coordinateParams(lat: Latitude, lon: Longitude) = Map(
+    "lat" -> lat.show,
+    "lon" -> lon.show
+  )
+
+  test("Request with valid latitude and longitude returns status code 200") {
+    val lat = Latitude(100f)
+    val lon = Longitude(100f)
+
+    val validReq = Request[IO](Method.GET, uri"/weather".withQueryParams(coordinateParams(lat, lon)))
+
+    assertIO(weatherRoute(validReq).map(_.status),Status.Ok)
   }
 
-  private[this] val retHelloWorld: IO[Response[IO]] = {
-    val getHW = Request[IO](Method.GET, uri"/hello/world")
-    val helloWorld = HelloWorld.impl
-    WeatherappRoutes.helloWorldRoutes(helloWorld).orNotFound(getHW)
-  }*/
+  private[this] def weatherRoute(req: Request[IO]): IO[Response[IO]] = {
+    val mockWeatherResponse = WeatherResponse(
+      Latitude(50f),
+      Longitude(50f),
+      List(
+        WeatherCondition("Rain")
+      ),
+      Temperature(280f)
+    )
+    val weatherClient = mockWeatherClient(mockWeatherResponse)
+    WeatherappRoutes.weatherRoutes(weatherClient).orNotFound(req)
+  }
 }
